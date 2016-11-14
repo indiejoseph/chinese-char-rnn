32a0ed3aa0bb7d8386b9e2359e6348c120151a93
Joseph Cheng <indiejoseph@gmail.com>
2016-11-14 22:49:27 +0800

    Update perplexity

--- models/charrnn.py
+++ models/charrnn.py
@@ -45,12 +45,7 @@ class CharRNN(Model):
         inputs = tf.split(1, seq_length, tf.nn.embedding_lookup(self.embedding, self.input_data))
         inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
-    def loop(prev, _):
-      prev = tf.matmul(prev, softmax_w, transpose_b=True) + softmax_b
-      prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
-      return tf.nn.embedding_lookup(self.embedding, prev_symbol)
-
-    outputs, last_state = seq2seq.rnn_decoder(inputs, self.initial_state, self.cell, loop_function=loop if infer else None, scope='rnnlm')
+    outputs, last_state = tf.nn.rnn(self.cell, inputs, initial_state=self.initial_state)
     output = tf.reshape(tf.concat(1, outputs), [-1, rnn_size])
     self.logits = tf.matmul(output, softmax_w, transpose_b=True) + softmax_b
     self.probs = tf.nn.softmax(self.logits)
