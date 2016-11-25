87ee90da1dd3a4f12fdccbf0c76bbdd2bdffe39a
Joseph Cheng <indiejoseph@gmail.com>
2016-11-26 01:36:54 +0800

    Update model config

--- models/charrnn.py
+++ models/charrnn.py
@@ -44,10 +44,7 @@ class CharRNN(Model):
 
       with tf.device("/cpu:0"):
         self.embedding = tf.get_variable("embedding",
-                                         [vocab_size, rnn_size],
-                                         initializer=tf.truncated_normal_initializer(
-                                           stddev=float(1.0 / np.sqrt(rnn_size))
-                                         ))
+                                         initializer=tf.random_uniform([vocab_size, rnn_size], -0.1, 1.0))
         inputs = tf.nn.embedding_lookup(self.embedding, self.input_data)
 
     outputs, self.final_state = tf.nn.dynamic_rnn(self.cell,
