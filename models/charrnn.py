3071b9f9b87a13dcc9507bd7a56ca639040a01cb
Joseph Cheng <indiejoseph@gmail.com>
2017-02-20 16:34:55 +0800

    Update hyper-parameters, remove embedding level dropuout

--- models/charrnn.py
+++ models/charrnn.py
@@ -43,9 +43,6 @@ class CharRNN(Model):
                                          initializer=tf.random_uniform_initializer(-stdv, stdv))
         inputs = tf.nn.embedding_lookup(self.embedding, self.input_data)
 
-        if is_training and keep_prob < 1:
-          inputs = tf.nn.dropout(inputs, self.keep_prob)
-
     self.initial_state = cell.zero_state(batch_size, tf.float32)
 
     with tf.variable_scope("output"):
