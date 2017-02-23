8260fc9792cefc57b237300a8d7749858ed6f2ac
Joseph Cheng <indiejoseph@gmail.com>
2017-02-23 15:31:51 +0800

    Add Multiplicated Integrated to rnn

--- models/charrnn.py
+++ models/charrnn.py
@@ -10,7 +10,7 @@ from rhm_cell import HighwayGRUCell
 
 class CharRNN(Model):
   def __init__(self, vocab_size=1000, batch_size=100,
-               layer_depth=2, rnn_size=1000,
+               layer_depth=2, rnn_size=1000, num_units=100,
                seq_length=50, keep_prob=0.9,
                grad_clip=5.0, num_sampled=5., is_training=True):
 
@@ -23,6 +23,7 @@ class CharRNN(Model):
     self.layer_depth = layer_depth
     self.keep_prob = keep_prob
     self.batch_size = batch_size
+    self.num_units = num_units
     self.seq_length = seq_length
     self.num_sampled = num_sampled
 
@@ -35,9 +36,14 @@ class CharRNN(Model):
                             use_recurrent_dropout=True,
                             is_training=is_training)
 
+      cell = rnn.OutputProjectionWrapper(cell, num_units)
+
+      if keep_prob < 1 and is_training:
+        cell = rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
+
       with tf.device("/cpu:0"):
         stdv = np.sqrt(1. / vocab_size)
-        self.embedding = tf.get_variable("embedding", [vocab_size, rnn_size],
+        self.embedding = tf.get_variable("embedding", [vocab_size, num_units],
                                          initializer=tf.random_uniform_initializer(-stdv, stdv))
         inputs = tf.nn.embedding_lookup(self.embedding, self.input_data)
 
@@ -51,13 +57,13 @@ class CharRNN(Model):
                                                     initial_state=self.initial_state,
                                                     dtype=tf.float32)
 
-      flat_output = tf.reshape(outputs, [-1, rnn_size])
+      flat_output = tf.reshape(outputs, [-1, num_units])
 
     with tf.variable_scope("loss"):
-      softmax_w = tf.get_variable("softmax_w", [rnn_size, vocab_size])
+      softmax_w = tf.get_variable("softmax_w", [num_units, vocab_size])
       softmax_b = tf.get_variable("softmax_b", [vocab_size])
 
-      loss = tf.nn.nce_loss(weights=tf.transpose(softmax_w), # weight tying
+      loss = tf.nn.nce_loss(weights=tf.transpose(softmax_w), # .T for some reason
                             biases=softmax_b,
                             inputs=flat_output,
                             labels=tf.reshape(self.targets, [-1, 1]), # Column vector
--- models/rhm_cell.py
+++ models/rhm_cell.py
@@ -2,7 +2,55 @@ import tensorflow as tf
 from tensorflow.python.ops.math_ops import tanh, sigmoid
 from tensorflow.contrib import rnn
 from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import variable_scope as vs
 from tensorflow.contrib.rnn.python.ops.core_rnn_cell_impl import _linear
+
+
+def mi_linear(arg1, arg2, output_size, global_bias_start=0.0, scope=None):
+  """Multiplicated Integrated Linear map:
+  See http://arxiv.org/pdf/1606.06630v1.pdf
+  A * (W[0] * arg1) * (W[1] * arg2) + (W[0] * arg1 * bias1) + (W[1] * arg2 * bias2) + global_bias.
+  Args:
+    arg1: batch x n, Tensor.
+    arg2: batch x n, Tensor.
+    output_size: int, second dimension of W[i].
+  global_bias_start: starting value to initialize the global bias; 0 by default.
+    scope: VariableScope for the created subgraph; defaults to "MILinear".
+  Returns:
+    A 2D Tensor with shape [batch x output_size] equal to
+    sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+  Raises:
+    ValueError: if some of the arguments has unspecified or wrong shape.
+  """
+  if arg1 is None:
+    raise ValueError("`arg1` must be specified")
+  if arg2 is None:
+    raise ValueError("`arg2` must be specified")
+  if output_size is None:
+    raise ValueError("`output_size` must be specified")
+
+  a1_shape = arg1.get_shape().as_list()[1]
+  a2_shape = arg2.get_shape().as_list()[1]
+
+  # Computation.
+  with vs.variable_scope(scope or "MILinear"):
+    matrix1 = vs.get_variable("Matrix1", [a1_shape, output_size])
+    matrix2 = vs.get_variable("Matrix2", [a2_shape, output_size])
+    bias1 = vs.get_variable("Bias1", [1, output_size],
+                 initializer=init_ops.constant_initializer(0.5))
+    bias2 = vs.get_variable("Bias2", [1, output_size],
+                 initializer=init_ops.constant_initializer(0.5))
+    alpha = vs.get_variable("Alpha", [output_size],
+                initializer=init_ops.constant_initializer(2.0))
+    arg1mul = math_ops.matmul(arg1, matrix1)
+    arg2mul = math_ops.matmul(arg2, matrix2)
+    res = alpha * arg1mul * arg2mul + (arg1mul * bias1) + (arg2mul * bias2)
+    global_bias_term = vs.get_variable(
+        "GlobalBias", [output_size],
+        initializer=init_ops.constant_initializer(global_bias_start))
+  return res + global_bias_term
 
 
 class HighwayGRUCell(rnn.RNNCell):
@@ -37,18 +85,18 @@ class HighwayGRUCell(rnn.RNNCell):
 
       with tf.variable_scope('h_'+str(highway_layer)):
         if highway_layer == 0:
-          h = _linear([inputs, current_state], self._num_units, True)
+          h = mi_linear(inputs, current_state, self._num_units, True)
         else:
           h = _linear([current_state], self._num_units, True)
 
         if self.is_training and self.use_recurrent_dropout:
-          h = tf.nn.dropout(h, keep_prob=self.dropout_keep_prob)
+          h = tf.nn.dropout(h, self.dropout_keep_prob)
 
         h = tf.tanh(h)
 
       with tf.variable_scope('t_'+str(highway_layer)):
         if highway_layer == 0:
-          t = tf.sigmoid(_linear([inputs, current_state], self._num_units, True, self.forget_bias))
+          t = tf.sigmoid(mi_linear(inputs, current_state, self._num_units, True, self.forget_bias))
         else:
           t = tf.sigmoid(_linear([current_state], self._num_units, True, self.forget_bias))
 
