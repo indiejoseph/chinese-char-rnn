f95f32921f8bb00bf3f3c2ef15579bd3a5406745
Joseph Cheng <indiejoseph@gmail.com>
2017-03-02 00:04:00 +0800

    Update train

--- models/lstm.py
+++ models/lstm.py
@@ -1,6 +1,10 @@
 import math
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.ops.math_ops import sigmoid
+from tensorflow.python.ops.math_ops import tanh
+from tensorflow.python.ops import array_ops
+from tensorflow.contrib.rnn import LSTMStateTuple
 
 try:
   from tensorflow.contrib.rnn import RNNCell
@@ -8,89 +12,55 @@ except ImportError:
   RNNCell = tf.nn.rnn_cell.RNNCel
 
 
-class LSTMCell(RNNCell):
-    """Vanilla LSTM implemented with same initializations as BN-LSTM"""
-    def __init__(self, num_units):
-        self.num_units = num_units
-
-    @property
-    def state_size(self):
-        return (self.num_units, self.num_units)
-
-    @property
-    def output_size(self):
-        return self.num_units
-
-    def __call__(self, x, state, scope=None):
-        with tf.variable_scope(scope or type(self).__name__):
-            c, h = state
-
-            # Keep W_xh and W_hh separate here as well to reuse initialization methods
-            x_size = x.get_shape().as_list()[1]
-            W_xh = tf.get_variable('W_xh',
-                [x_size, 4 * self.num_units],
-                initializer=orthogonal_initializer())
-            W_hh = tf.get_variable('W_hh',
-                [self.num_units, 4 * self.num_units],
-                initializer=bn_lstm_identity_initializer(0.95))
-            bias = tf.get_variable('bias', [4 * self.num_units])
-
-            # hidden = tf.matmul(x, W_xh) + tf.matmul(h, W_hh) + bias
-            # improve speed by concat.
-            concat = tf.concat(1, [x, h])
-            W_both = tf.concat(0, [W_xh, W_hh])
-            hidden = tf.matmul(concat, W_both) + bias
-
-            i, j, f, o = tf.split(1, 4, hidden)
-
-            new_c = c * tf.sigmoid(f) + tf.sigmoid(i) * tf.tanh(j)
-            new_h = tf.tanh(new_c) * tf.sigmoid(o)
-
-            return new_h, (new_c, new_h)
-
 class BNLSTMCell(RNNCell):
     """Batch normalized LSTM as described in http://arxiv.org/abs/1603.09025"""
-    def __init__(self, num_units, training):
-        self.num_units = num_units
-        self.training = training
+    def __init__(self, num_units, forget_bias=1.0, activation=tanh, reuse=None, training=False):
+        self._num_units = num_units
+        self._forget_bias = forget_bias
+        self._activation = activation
+        self._reuse = reuse
+        self._training = training
 
     @property
     def state_size(self):
-        return (self.num_units, self.num_units)
+        return LSTMStateTuple(self._num_units, self._num_units)
 
     @property
     def output_size(self):
-        return self.num_units
+        return self._num_units
 
-    def __call__(self, x, state, scope=None):
-        with tf.variable_scope(scope or 'bn_lstm'):
-            c, h = state
+    def __call__(self, inputs, state, scope=None):
+        """Long short-term memory cell (LSTM)."""
+        with tf.variable_scope(scope or type(self).__name__, reuse=self._reuse):
+          # Parameters of gates are concatenated into one multiply for efficiency.
+          c, h = state
+          input_size = inputs.get_shape().as_list()[1]
+          W_xh = tf.get_variable('W_xh',
+              [input_size, 4 * self._num_units],
+              initializer=orthogonal_initializer())
+          W_hh = tf.get_variable('W_hh',
+              [self._num_units, 4 * self._num_units],
+              initializer=bn_lstm_identity_initializer(0.95))
+          bias = tf.get_variable('bias', [4 * self._num_units])
 
-            x_size = x.get_shape().as_list()[1]
-            W_xh = tf.get_variable('W_xh',
-                [x_size, 4 * self.num_units],
-                initializer=orthogonal_initializer())
-            W_hh = tf.get_variable('W_hh',
-                [self.num_units, 4 * self.num_units],
-                initializer=bn_lstm_identity_initializer(0.95))
-            bias = tf.get_variable('bias', [4 * self.num_units])
+          xh = tf.matmul(inputs, W_xh)
+          hh = tf.matmul(h, W_hh)
 
-            xh = tf.matmul(x, W_xh)
-            hh = tf.matmul(h, W_hh)
+          bn_xh = batch_norm(xh, 'xh', self._training)
+          bn_hh = batch_norm(hh, 'hh', self._training)
 
-            bn_xh = batch_norm(xh, 'xh', self.training)
-            bn_hh = batch_norm(hh, 'hh', self.training)
+          hidden = bn_xh + bn_hh + bias
 
-            hidden = bn_xh + bn_hh + bias
+          # i = input_gate, j = new_input, f = forget_gate, o = output_gate
+          i, j, f, o = array_ops.split(value=hidden, num_or_size_splits=4, axis=1)
 
-            i, j, f, o = tf.split(hidden, 4, axis=1)
+          new_c = (c * sigmoid(f + self._forget_bias) + sigmoid(i) *
+                   self._activation(j))
+          bn_new_c = batch_norm(new_c, 'c', self._training)
+          new_h = self._activation(bn_new_c) * sigmoid(o)
+          new_state = LSTMStateTuple(new_c, new_h)
 
-            new_c = c * tf.sigmoid(f) + tf.sigmoid(i) * tf.tanh(j)
-            bn_new_c = batch_norm(new_c, 'c', self.training)
-
-            new_h = tf.tanh(bn_new_c) * tf.sigmoid(o)
-
-            return new_h, (new_c, new_h)
+          return new_h, new_state
 
 def orthogonal(shape):
     flat_shape = (shape[0], np.prod(shape[1:]))
